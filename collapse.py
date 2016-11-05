39d29a0c82ce619cd3a05137c4f868d426815db7
Diego Mallo <adamallo@gmail.com>
2016-11-05 02:02:27 +0100

    Bugfix of collapse.py and new script to conduct mrl analyses

--- collapse.py
+++ collapse.py
@@ -22,36 +22,11 @@ for specie in species:
 	regex=re.compile(specie + " .+ .+")
 	leaves=[match.group(0) for label in labels for match in [regex.match(label)] if match]
 	mrca_node=tree.mrca(taxon_labels=leaves)
-	mrca_node.clear_child_nodes()
+	del mrca_node._child_nodes[:]
 	taxon=Taxon(specie)
 	mrca_node.taxon=taxon
 	newNamespace.add_taxon(taxon)
 
 tree.taxon_namespace=newNamespace
 tree.write(path=args.o,schema="newick",suppress_rooting=True)
-#
-#
-#if args.gt != 0:
-#        print "Scaling branch lengths to time with generation time %d\n" % args.gt
-#        for tree in trees:
-#                for edge in tree.preorder_edge_iter():
-#                        #print "DEBUG: %s" % edge.length
-#                        if edge.length != None:
-#                                edge.length=edge.length/args.gt
-#
-#if args.od != 0:
-#        print "Adding outgroup with branch length %d\n" % args.od
-#        namespace=trees.taxon_namespace
-#        outgroup= Taxon("outgroup")
-#        namespace.add_taxon(outgroup)
-#        ntree=0
-#        labels=namespace.labels()
-#        labels.remove("outgroup")
-#        for tree in trees:
-#                outgroup_node=Node(taxon=outgroup,edge_length=args.od)
-#                new_root_node=Node()
-#                tree.seed_node.edge_length=args.od-tree.seed_node.distance_from_tip()
-#                new_root_node.add_child(tree.seed_node)
-#                new_root_node.add_child(outgroup_node)
-#                tree.seed_node=new_root_node
 
